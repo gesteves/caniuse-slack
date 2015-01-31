2b24b94c76fc0d0c48ee9f43daf4e576673a0e49
Guillermo Esteves <g@gesteves.com>
2015-01-31 16:49:47 -0500

    Add a couple linebreaks

--- app.rb
+++ app.rb
@@ -127,7 +127,7 @@ def build_attachment(key, feature)
   attachment[:color] = get_attachment_color(feature)
   attachment[:title] = feature["title"]
   attachment[:title_link] = "http://caniuse.com/#feat=#{key}"
-  attachment[:text] = feature["description"]
+  attachment[:text] = feature["description"] + "\n\n"
   attachment[:fallback] = "#{feature["title"]} (http://caniuse.com/#feat=#{key}): #{feature["description"]}"
   attachment[:mrkdwn_in] = ["text", "title", "fields", "fallback"]
   fields = []
